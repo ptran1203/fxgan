bfabceb12611f35ccff325e4749a9fd5a45958c1
ubuntu <phatth1203@gmail.com>
2020-03-30 10:38:10 +0700

    ffa

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@
 now=$(date +'%Y-%m-%d-%H:%M:%S')
 git add .
 git commit -m "$now"
-git ps
+git push
 echo $now
--- relation_net_keras.py
+++ relation_net_keras.py
@@ -1,4 +1,4 @@
-
+%tensorflow_version 1.x
 
 import sys
 import os
@@ -18,60 +18,158 @@ from keras.optimizers import Adam
 from keras.layers import (
     Input, Dense, Reshape,
     Flatten, Embedding, Dropout,
-    BatchNormalization,Activation
+    BatchNormalization, Activation,
+    Lambda,Layer, Add, Concatenate
 )
+from keras.utils import np_utils, plot_model
+
+
 from keras.layers import multiply as kmultiply
 from keras.layers import add as kadd
 import csv
 from PIL import Image
+from google.colab import drive
+
+drive.mount('/content/drive')
+
+BASE_DIR = '/content/drive/My Drive/bagan'
+
+def pickle_load(path):
+    try:
+        with open(path, "rb") as f:
+            return pickle.load(f)
+    except:
+        return None
+
+def load_ds(rst, opt):
+    path = '{}/imgs_{}_{}.pkl'.format('/content/drive/My Drive/bagan/dataset/save', opt, rst)
+    return pickle_load(path)
+
+def generate_data(dataset, n_way, k_shot):
+    query_size = 15
+    train_x, train_y = dataset
+    idx = np.random.choice(
+                train_y,
+                size = (query_size,)
+                )
+    
+    query_imgs = train_x[idx]
+    train_x = np.delete(train_x, idx)
+    train_y = np.delete(train_y, idx)
+    
+    idx0 = np.where(train_y == 0)[0]
+    idx1 = np.where(train_y == 1)[0]
+
+    np.random.shuffle(idx0)
+    np.random.shuffle(idx1)
+
+    idx0 = idx0[:k_shot]
+    idx1 = idx1[:k_shot]
+
+    c_idx = np.concatenate((idx0, idx1))
+
+    train_x = train_x[c_idx]
+    train_y = train_y[c_idx]
+
+    return (train_x, train_y), query_imgs
+
+class RelationNet():
+    def __init__(self, img_rst, dataset):
+        self.rst = img_rst
+        self.build_model()
+
+    def _embedding_module(self, x):
+        # input_tensor = Input((img_size, img_size, 1))
+        x = Conv2D(64, (3, 3))(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+        x = Conv2D(64, (3, 3))(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+        x = Conv2D(64, (3, 3))(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+        x = Conv2D(64, (3, 3))(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+        return x
+
+    def _relation_module(self):
+        model = Sequential()
+        model.add(Conv2D(64, (3, 3), strides = 2))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Conv2D(64, (3, 3)))
+        model.add(BatchNormalization())
+        model.add(Activation('relu'))
+        model.add(Flatten())
+        # relation score
+        model.add(Dense(2, activation='sigmoid', name='sigmoid_output'))
+        return model
+
+    def build_model(self):
+        img_size = self.rst
+        # 2 classes
+        c_way = 2
+        k_shot = 5
+        support_size = c_way * k_shot
+
+        # support set and 1 query img
+        # each 5 images related to 1 class
+        imgs = Input((support_size + 1, img_size, img_size, 1))
+        idx = 0
+        raw_features = [[], []]
+
+        for c in range(c_way):
+            for i in range(k_shot):
+                # print(type(embedding_net))
+                xx = self._embedding_module(Lambda(lambda x: x[:,idx,:,:,:])(imgs))
+                raw_features[c].append(xx)
+                idx += 1
+
+        query_feature = self._embedding_module(Lambda(lambda x: x[:,-1,:,:,:])(imgs))
+
+        # one relation net give 1 relation score
+        features = Add()(raw_features[0])
+        # print(features)
+        features = Concatenate()([features, query_feature])
+        for i in range(1, c_way):
+            feature = Add()(raw_features[i])
+            feature = Concatenate()([feature, query_feature])
+            features = Concatenate()([features, feature])
+        
+        relation_scores = self._relation_module()(features)
+
+        self.model = Model(inputs = imgs, outputs = relation_scores)
+        self.model.compile(optimizer = 'adam', loss = 'binary_cross_entropy')
+
+    def plot(self):
+        plot_model(self.model, to_file='/content/drive/My Drive/bagan/rn_model.png')
+
+    def summary(self):
+        self.model.summary()
+
+    def train(self, dataset, query_imgs, epochs = 100):
+        train_x, train_y = dataset
+        q_x, q_y = query_imgs
+ 
+        for i in range(epochs):
+            q_idx = np.random.randint(0, len(q_y) - 1)
+            query_img = q_x[q_idx]
+            query_label = q_y[q_idx]
+
+            train_x = np.concatenate((train_x, query_img))
+            # train_x = np.concatenate((train_x, query_img))
+
+            loss = self.model.train_on_batch(train_x, np.array([0, 1]))
 
 
-# Hyper Parameters
-FEATURE_DIM = 64
-RELATION_DIM = 8
-CLASS_NUM = 2
-SAMPLE_NUM_PER_CLASS = 100
-BATCH_NUM_PER_CLASS = 10
-EPISODE = 5000
-TEST_EPISODE = 600
-LEARNING_RATE = 0.001
-HIDDEN_UNIT = 10
 
 
-def embedding_module(img_size, hidden_size):
-    # input
-    input_tensor = Input((img_size, img_size, 1))
+rn_model = RelationNet(64, '')
+rn_model.summary()
 
-    x = Conv2D(64, (3, 3))(input_tensor)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-
-    x = Conv2D(64, (3, 3))(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-
-    x = Conv2D(64, (3, 3))(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-
-    x = Conv2D(64, (3, 3))(x)
-    x = BatchNormalization()(x)
-    x = Activation('relu')(x)
-
-    features = Flatten()(x)
-    return Dense(hidden_size)(features)
-
-def relation_module(input_size, hidden_size):
-    # input is vector
-    model = Sequential()
-    model.add(Dense(hidden_size, input_shape=(input_size,)))
-    model.add(Conv2D(64, (3, 3), strides = 2))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv2D(64, (3, 3)))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Flatten())
-    # relation score
-    model.add(Activation('sigmoid'))
-    return model
