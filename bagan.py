17e669ab4fff7ab9051cc4602bf58b0641291895
phattran <phatth1203@gmail.com>
2020-04-14 22:28:54 +0700

    2020-04-14-22:28:54

--- bagan.py
+++ bagan.py
@@ -1018,30 +1018,30 @@ class BalancingGAN:
         self.discriminator.load_weights(fname_discriminator)
 
     def save_image_array(self, img_array, fname=None, show=None):
-    channels = img_array.shape[-1]
-    resolution = img_array.shape[2]
-    img_rows = img_array.shape[0]
-    img_cols = img_array.shape[1]
+        channels = img_array.shape[-1]
+        resolution = img_array.shape[2]
+        img_rows = img_array.shape[0]
+        img_cols = img_array.shape[1]
 
-    img = np.full([channels, resolution * img_rows, resolution * img_cols], 0.0)
-    for r in range(img_rows):
-        for c in range(img_cols):
-            img[:,
-            (resolution * r): (resolution * (r + 1)),
-            (resolution * (c % 10)): (resolution * ((c % 10) + 1))
-            ] = img_array[r, c]
+        img = np.full([channels, resolution * img_rows, resolution * img_cols], 0.0)
+        for r in range(img_rows):
+            for c in range(img_cols):
+                img[:,
+                (resolution * r): (resolution * (r + 1)),
+                (resolution * (c % 10)): (resolution * ((c % 10) + 1))
+                ] = img_array[r, c]
 
-    img = (img * 127.5 + 127.5).astype(np.uint8)
-    if (img.shape[0] == 1):
-        img = img[0]
-    else:
-        img = np.rollaxis(img, 0, 3)
+        img = (img * 127.5 + 127.5).astype(np.uint8)
+        if (img.shape[0] == 1):
+            img = img[0]
+        else:
+            img = np.rollaxis(img, 0, 3)
 
-    if show:
-        try:
-            cv2_imshow(img)
-        except Exception as e:
-            print('[show fail] ', str(e))
-    if fname:
-        Image.fromarray(img).save(fname)
+        if show:
+            try:
+                cv2_imshow(img)
+            except Exception as e:
+                print('[show fail] ', str(e))
+        if fname:
+            Image.fromarray(img).save(fname)
 
