a01c46769180fc695050fbbbbc44242a2ef2d9d7
phattran <phatth1203@gmail.com>
2020-04-17 01:06:32 +0700

    2020-04-17-01:06:32

--- bagan.py
+++ bagan.py
@@ -772,8 +772,8 @@ class BalancingGAN:
         layers_r = self.reconstructor.layers
         layers_d = self.discriminator.layers
 
-        # for l in range(1, len(layers_r)-1):
-            # layers_d[l].set_weights( layers_r[l].get_weights() )
+        for l in range(1, len(layers_r)-1):
+            layers_d[l].set_weights( layers_r[l].get_weights() )
 
         # Organize multivariate distribution
         if not multivariate_prelearnt:
@@ -912,33 +912,52 @@ class BalancingGAN:
                 # Test: # generate a new batch of noise
                 nb_test = bg_test.get_num_samples()
                 fake_size = int(np.ceil(nb_test * 1.0/self.nclasses))
-                sampled_labels = self._biased_sample_labels(nb_test, "d")
+                sampled_labels = self._biased_sample_labels(nb_test + self.k_shot, "d")
                 latent_gen = self.generate_latent(sampled_labels, bg_test)
             
                 # sample some labels from p_c and generate images from them
                 generated_images = self.generator.predict(
                     latent_gen, verbose=False)
+
+                fake_images = generated_images[self.k_shot:]
+                support_fakes = generated_images[:self.k_shot]
+
+                generated_images[0] = (generated_images[0] * 127.5 + 127.5).astype(np.uint8)
+                cv2_imshow(generated_images[0])
             
-                X = np.concatenate( (bg_test.dataset_x, generated_images) )
-                aux_y = np.concatenate((bg_test.dataset_y, np.full(len(sampled_labels), self.nclasses )), axis=0)
-            
+                X = np.concatenate( (bg_test.dataset_x, fake_images) )
+                aux_y = np.concatenate((bg_test.dataset_y, np.full(len(fake_images), self.nclasses )), axis=0)
+                aux_y = np_utils.to_categorical(aux_y, self.nclasses + 1)
+                support_images = bg_train.merge_support_images(support_fakes, X.shape[0])
                 # see if the discriminator can figure itself out...
-                # test_disc_loss, test_disc_acc = self.discriminator.evaluate(
-                #     [support_images, X], aux_y, verbose=False)
+                test_disc_loss, test_disc_acc = self.discriminator.evaluate(
+                    [support_images, X], aux_y, verbose=False)
             
                 # make new latent
                 sampled_labels = self._biased_sample_labels(fake_size + nb_test, "g")
                 latent_gen = self.generate_latent(sampled_labels, bg_test)
 
-                # test_gen_loss, test_gen_acc = self.combined.evaluate(
-                #     latent_gen,
-                #     sampled_labels, verbose=False)
+                support_images = bg_train.merge_support_images(support_fakes,
+                                                               sampled_labels.shape[0])
+
+                test_gen_loss, test_gen_acc = self.combined.evaluate(
+                    [latent_gen, support_images],
+                    sampled_labels, verbose=False)
 
 
                 print("D_loss {}, G_loss {}, D_acc {}, G_acc {} - {}".format(
                     train_disc_loss, train_gen_loss, train_disc_acc, train_gen_acc,
                     datetime.datetime.now() - start_time
                 ))
+                self.train_history['disc_loss'].append(train_disc_loss)
+                self.train_history['gen_loss'].append(train_gen_loss)
+                self.test_history['disc_loss'].append(test_disc_loss)
+                self.test_history['gen_loss'].append(test_gen_loss)
+                # accuracy
+                self.train_history['disc_acc'].append(train_disc_acc)
+                self.train_history['gen_acc'].append(train_gen_acc)
+                self.test_history['disc_acc'].append(test_disc_acc)
+                self.test_history['gen_acc'].append(test_gen_acc)
                 # self.plot_his()
 
                 # Save sample images
