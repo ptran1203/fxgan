0a9454357012cdb017853f069ceedd1edfeda0a6
phattran <phatth1203@gmail.com>
2020-04-19 22:49:26 +0700

    2020-04-19-22:49:26

--- bagan_old.py
+++ bagan_old.py
@@ -370,7 +370,7 @@ class BalancingGAN:
         fake_image_from_latent = cnn(latent)
         self.generator = Model(inputs=latent, outputs=fake_image_from_latent, name = 'Generator')
 
-     def _build_common_encoder(self, image, min_latent_res=8):
+    def _build_common_encoder(self, image, min_latent_res=8):
         resolution = self.resolution
         channels = self.channels
 
