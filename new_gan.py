4117c4c2ca5fb1566c2c8b2f3b7efaacdb82e674
phattran <phatth1203@gmail.com>
2020-04-26 23:05:20 +0700

    2020-04-26-23:05:20

--- new_gan.py
+++ new_gan.py
@@ -314,6 +314,111 @@ class BatchGenerator:
             yield dataset_x[access_pattern, :, :, :], labels[access_pattern]
 
 class BalancingGAN:
+    def build_res_unet(
+        self,
+        filter_root = 32,
+        depth = 5,
+        n_class=1,
+        input_size=(64, 64, 1),
+        activation='relu',
+        batch_norm=True,
+        final_activation='tanh'
+    ):
+        """
+        Build UNet model with ResBlock.
+        Args:
+            filter_root (int): Number of filters to start with in first convolution.
+            depth (int): How deep to go in UNet i.e. how many down and up sampling you want to do in the model. 
+                        Filter root and image size should be multiple of 2^depth.
+            n_class (int, optional): How many classes in the output layer. Defaults to 2.
+            input_size (tuple, optional): Input image size. Defaults to (256, 256, 1).
+            activation (str, optional): activation to use in each convolution. Defaults to 'relu'.
+            batch_norm (bool, optional): To use Batch normaliztion or not. Defaults to True.
+            final_activation (str, optional): activation for output layer. Defaults to 'softmax'.
+        Returns:
+            obj: keras model object
+        """
+
+        latent = Input(shape = (100,))
+        inputs = Dense(input_size[0] * input_size[1] * input_size[2], input_dim=100)(latent)
+        inputs = BatchNormalization()(inputs)
+        inputs = LeakyReLU()(inputs)
+        inputs = Reshape(input_size)(inputs)
+
+        x = inputs
+        # Dictionary for long connections
+        long_connection_store = {}
+
+        Conv = Conv2D
+        MaxPooling = MaxPooling2D
+        UpSampling = UpSampling2D
+
+        # Down sampling
+        for i in range(depth):
+            out_channel = 2**i * filter_root
+
+            # Residual/Skip connection
+            res = Conv(out_channel, kernel_size=1, padding='same', use_bias=False, name="Identity{}_1".format(i))(x)
+
+            # First Conv Block with Conv, BN and activation
+            conv1 = Conv(out_channel, kernel_size=3, padding='same', name="Conv{}_1".format(i))(x)
+            if batch_norm:
+                conv1 = BatchNormalization(name="BN{}_1".format(i))(conv1)
+            act1 = Activation(activation, name="Act{}_1".format(i))(conv1)
+
+            # Second Conv block with Conv and BN only
+            conv2 = Conv(out_channel, kernel_size=3, padding='same', name="Conv{}_2".format(i))(act1)
+            if batch_norm:
+                conv2 = BatchNormalization(name="BN{}_2".format(i))(conv2)
+
+            resconnection = Add(name="Add{}_1".format(i))([res, conv2])
+
+            act2 = Activation(activation, name="Act{}_2".format(i))(resconnection)
+
+            # Max pooling
+            if i < depth - 1:
+                long_connection_store[str(i)] = act2
+                x = MaxPooling(padding='same', name="MaxPooling{}_1".format(i))(act2)
+            else:
+                x = act2
+
+        # Upsampling
+        for i in range(depth - 2, -1, -1):
+            out_channel = 2**(i) * filter_root
+
+            # long connection from down sampling path.
+            long_connection = long_connection_store[str(i)]
+
+            up1 = UpSampling(name="UpSampling{}_1".format(i))(x)
+            up_conv1 = Conv(out_channel, 2, activation='relu', padding='same', name="upConv{}_0".format(i))(up1)
+
+            #  Concatenate.
+            up_conc = Concatenate(axis=-1, name="upConcatenate{}_1".format(i))([up_conv1, long_connection])
+
+            #  Convolutions
+            up_conv2 = Conv(out_channel, 3, padding='same', name="upConv{}_1".format(i))(up_conc)
+            if batch_norm:
+                up_conv2 = BatchNormalization(name="upBN{}_1".format(i))(up_conv2)
+            up_act1 = Activation(activation, name="upAct{}_1".format(i))(up_conv2)
+
+            up_conv2 = Conv(out_channel, 3, padding='same', name="upConv{}_2".format(i))(up_act1)
+            if batch_norm:
+                up_conv2 = BatchNormalization(name="upBN{}_2".format(i))(up_conv2)
+
+            # Residual/Skip connection
+            res = Conv(out_channel, kernel_size=1, padding='same', use_bias=False, name="upIdentity{}_1".format(i))(up_conc)
+
+            resconnection = Add(name="upAdd{}_1".format(i))([res, up_conv2])
+
+            x = Activation(activation, name="upAct{}_2".format(i))(resconnection)
+
+        # Final convolution
+        output = Conv(n_class, 1, padding='same', activation=final_activation, name='output')(x)
+
+        self.generator = Model(latent, outputs=output, name='Res-UNet')
+
+
+
     def plot_loss_his(self):
         train_d = self.train_history['disc_loss']
         train_g = self.train_history['gen_loss']
@@ -473,6 +578,10 @@ class BalancingGAN:
             output = model_output
         )
 
+    def build_g_trigger(self):
+            self.build_res_unet()
+            # self.build_generator(self.latent_size, self.min_latent_res)
+
     def __init__(self, classes, target_class_id,
                 # Set dratio_mode, and gratio_mode to 'rebalance' to bias the sampling toward the minority class
                 # No relevant difference noted
@@ -502,9 +611,10 @@ class BalancingGAN:
         self.trained = False
 
         # Build generator
-        self.build_generator(latent_size, init_resolution=min_latent_res)
+        # self.build_generator(latent_size, init_resolution=min_latent_res)
+        self.build_g_trigger()
         self.generator.compile(
-            optimizer=Adam(lr=self.adam_lr, beta_1=self.adam_beta_1),
+            optimizer=Adam(lr=self.g_lr, beta_1=self.adam_beta_1),
             loss='sparse_categorical_crossentropy'
         )
 
