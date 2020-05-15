55965d9d079216e930794da8a78a60904c6ccc61
phattran <phatth1203@gmail.com>
2020-05-15 14:55:38 +0700

    2020-05-15-14:55:38

--- new_gan.py
+++ new_gan.py
@@ -263,7 +263,7 @@ class BatchGenerator:
             self.dataset_x = np.transpose(self.dataset_x, axes=(0, 3, 2, 1))
         # Normalize between -1 and 1
         self.dataset_x = (self.dataset_x - 127.5) / 127.5
-        if self.data_src == self.TRAIN:
+        if self.data_src == self.TRAIN or rst == 64:
             self.dataset_x = np.expand_dims(self.dataset_x, axis = -1)
 
         assert (self.dataset_x.shape[0] == self.dataset_y.shape[0])
