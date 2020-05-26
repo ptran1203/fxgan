d710c9433b29b74234a00242109759573ed94c9e
phattran <phatth1203@gmail.com>
2020-05-26 12:20:27 +0700

    2020-05-26-12:20:27

--- new_gan.py
+++ new_gan.py
@@ -826,7 +826,7 @@ class BalancingGAN:
 
         features = Dropout(0.4)(features)
         aux = Dense(
-            2, activation='sigmoid', name='auxiliary' # use hinge loss
+            1, activation='sigmoid', name='auxiliary' # use hinge loss
         )(features)
 
         self.discriminator = Model(inputs=image, outputs=aux, name='discriminator')
