511ece5c900faff67c30d7301e9e5ee70c8f938a
phattran <phatth1203@gmail.com>
2020-05-31 00:26:31 +0700

    PUSH FROM WINDOW

--- new_gan.py
+++ new_gan.py
@@ -474,6 +474,67 @@ class RandomPick(keras.layers.Layer):
         return input_shape[0]
 
 
+class SelfAttention(Layer):
+    def __init__(self, ch, **kwargs):
+        super(SelfAttention, self).__init__(**kwargs)
+        self.channels = ch
+        self.filters_f_g = self.channels // 8
+        self.filters_h = self.channels
+
+    def build(self, input_shape):
+        kernel_shape_f_g = (1, 1) + (self.channels, self.filters_f_g)
+        kernel_shape_h = (1, 1) + (self.channels, self.filters_h)
+
+        # Create a trainable weight variable for this layer:
+        self.gamma = self.add_weight(name='gamma', shape=[1], initializer='zeros', trainable=True)
+        self.kernel_f = self.add_weight(shape=kernel_shape_f_g,
+                                        initializer='glorot_uniform',
+                                        name='kernel_f',
+                                        trainable=True)
+        self.kernel_g = self.add_weight(shape=kernel_shape_f_g,
+                                        initializer='glorot_uniform',
+                                        name='kernel_g',
+                                        trainable=True)
+        self.kernel_h = self.add_weight(shape=kernel_shape_h,
+                                        initializer='glorot_uniform',
+                                        name='kernel_h',
+                                        trainable=True)
+
+        super(SelfAttention, self).build(input_shape)
+        # Set input spec.
+        self.input_spec = keras.layers.InputSpec(ndim=4,
+                                    axes={3: input_shape[-1]})
+        self.built = True
+
+    def call(self, x):
+        def hw_flatten(x):
+            return K.reshape(x, shape=[K.shape(x)[0], K.shape(x)[1]*K.shape(x)[2], K.shape(x)[3]])
+
+        f = K.conv2d(x,
+                     kernel=self.kernel_f,
+                     strides=(1, 1), padding='same')  # [bs, h, w, c']
+        g = K.conv2d(x,
+                     kernel=self.kernel_g,
+                     strides=(1, 1), padding='same')  # [bs, h, w, c']
+        h = K.conv2d(x,
+                     kernel=self.kernel_h,
+                     strides=(1, 1), padding='same')  # [bs, h, w, c]
+
+        s = K.batch_dot(hw_flatten(g), K.permute_dimensions(hw_flatten(f), (0, 2, 1)))  # # [bs, N, N]
+
+        beta = K.softmax(s, axis=-1)  # attention map
+
+        o = K.batch_dot(beta, hw_flatten(h))  # [bs, N, C]
+
+        o = K.reshape(o, shape=K.shape(x))  # [bs, h, w, C]
+        x = self.gamma * o + x
+
+        return x
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+
 class FeatureNorm(keras.layers.Layer):
     def __init__(self, epsilon = 1e-6, norm = 'batch'):
         super(FeatureNorm, self).__init__()
@@ -765,10 +826,10 @@ class BalancingGAN:
         latent_noise3 = Dense(hw*hw*64,)(latent_code)
         latent_noise3 = Reshape((hw, hw, 64))(latent_noise3)
 
-        # en_1 = feature[0]
-        en_2 = feature[1]
-        en_3 = feature[2]
-        en_4 = feature[3]
+        en_1 = SelfAttention(64)(feature[0])
+        en_2 = SelfAttention(64)(feature[0])
+        en_3 = SelfAttention(128)(feature[0])
+        en_4 = SelfAttention(128)(feature[0])
 
         # en_4 = Concatenate()([en_4, latent_noise1])
         # en_3 = Concatenate()([en_3, latent_noise2])
@@ -777,18 +838,14 @@ class BalancingGAN:
         # botteneck
         decoder_activation = Activation('relu')
         de_1 = self._res_block(en_4, activation='relu', norm = 'feature', scale=scale, bias=bias)
-        # de_1 = self._res_block(en_4)
         de_1 = Conv2DTranspose(128, 5, strides = 2, padding = 'same')(de_1)
-        # de_1 = self._norm()(de_1)
         de_1 = decoder_activation(de_1)
         de_1 = FeatureNorm()([de_1, scale, bias])
         de_1 = Dropout(0.3)(de_1)
         de_1 = Add()([de_1, en_3])
 
         de_2 = self._res_block(de_1, activation='relu', norm = 'feature', scale=scale, bias=bias)
-        # de_2 = self._res_block(de_1)
         de_2 = Conv2DTranspose(64, 5, strides = 2, padding = 'same')(de_2)
-        # de_2 = self._norm()(de_2)
         de_2 = decoder_activation(de_2)
         de_2 = FeatureNorm()([de_2, scale, bias])
         de_2 = Dropout(0.3)(de_2)
@@ -796,11 +853,10 @@ class BalancingGAN:
 
         de_3 = self._res_block(de_2, activation='relu', norm = 'feature', scale=scale, bias=bias)
         de_3 = Conv2DTranspose(64, 5, strides = 2, padding = 'same')(de_3)
-        de_3 = self._norm()(de_3)
         de_3 = decoder_activation(de_3)
         de_3 = FeatureNorm()([de_3, scale, bias])
         de_3 = Dropout(0.3)(de_3)
-        de_3 = Add()([de_3, feature[0]])
+        de_3 = Add()([de_3, en_1])
 
         final = Conv2DTranspose(1, 5, strides = 2, padding = 'same')(de_3)
         outputs = Activation('tanh')(final)
