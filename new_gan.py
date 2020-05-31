a55dc8832589aa27adddfc72b23003e19ad3b03b
phattran <phatth1203@gmail.com>
2020-05-31 13:07:36 +0700

    2020-05-31-13:07:36

--- new_gan.py
+++ new_gan.py
@@ -836,7 +836,8 @@ class BalancingGAN:
 
         # botteneck
         decoder_activation = Activation('relu')
-        de_1 = self._res_block(en_4, activation='relu', norm = 'feature', scale=scale, bias=bias)
+        # de_1 = self._res_block(en_4, activation='relu', norm = 'feature', scale=scale, bias=bias)
+        de_1 = self._res_block(en_4, 'relu')
         de_1 = Conv2DTranspose(128, 5, strides = 2, padding = 'same')(de_1)
         de_1 = decoder_activation(de_1)
         # de_1 = FeatureNorm()([de_1, scale, bias])
@@ -846,7 +847,8 @@ class BalancingGAN:
 
         de_1 = SelfAttention(128)(de_1)
 
-        de_2 = self._res_block(de_1, activation='relu', norm = 'feature', scale=scale, bias=bias)
+        # de_2 = self._res_block(de_1, activation='relu', norm = 'feature', scale=scale, bias=bias)
+        de_2 = self._res_block(de_1, 'relu')
         de_2 = Conv2DTranspose(64, 5, strides = 2, padding = 'same')(de_2)
         de_2 = decoder_activation(de_2)
         # de_2 = FeatureNorm()([de_2, scale, bias])
@@ -854,7 +856,8 @@ class BalancingGAN:
         de_2 = Dropout(0.3)(de_2)
         de_2 = Add()([de_2, en_2])
 
-        de_3 = self._res_block(de_2, activation='relu', norm = 'feature', scale=scale, bias=bias)
+        # de_3 = self._res_block(de_2, activation='relu', norm = 'feature', scale=scale, bias=bias)
+        de_3 = self._res_block(de_2, 'relu')
         de_3 = Conv2DTranspose(64, 5, strides = 2, padding = 'same')(de_3)
         de_3 = decoder_activation(de_3)
         # de_3 = FeatureNorm()([de_3, scale, bias])
