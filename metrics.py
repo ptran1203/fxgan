5379b87cac16236313c59126aad547dda2d5d1a0
phattran <phatth1203@gmail.com>
2020-06-19 23:34:59 +0700

    fix

--- metrics.py
+++ metrics.py
@@ -2,17 +2,19 @@ from sklearn.metrics import roc_auc_score
 from keras.utils import to_categorical
 from const import CATEGORIES_MAP, INVERT_CATEGORIES_MAP
 
-def auc_score(y_true, y_pred, verbose=0, plot=0):
+def auc_score(y_true, y_pred, verbose=1, plot=0):
     # y_true is not one-hot
-    if len(y_true.shape != y_pred.shape):
+    if y_true.shape != y_pred.shape:
         _y_true = to_categorical(y_true, y_pred.shape[-1])
-        scores = roc_auc_score(_y_true, y_pre, average=None)
+        scores = roc_auc_score(_y_true, y_pred, average=None)
     else:
-        scores = roc_auc_score(y_true, y_pre, average=None)
+        scores = roc_auc_score(y_true, y_pred, average=None)
     
     if verbose:
         print('AUC score: \n')
-        for i in range(scores):
+        print(scores)
+        for i in range(len(scores)):
+            print(scores[i])
             print('{}: {}'.format(INVERT_CATEGORIES_MAP[i], scores[i])) 
 
     if plot:
