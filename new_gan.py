d7b7f3f03ae56a86cb154418343360dfae0a3ec4
phattran <phatth1203@gmail.com>
2020-06-22 23:02:52 +0700

    resblock

--- new_gan.py
+++ new_gan.py
@@ -188,6 +188,7 @@ class BalancingGAN:
         out = actv(out)
 
         out = Conv2D(K.int_shape(x)[-1], kernel_size, strides = 1, padding='same')(out)
+        out = norm_layer(out)
         out = actv(out)
         out = Add()([out, x])
         return out
