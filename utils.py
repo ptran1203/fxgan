6e4e6d29d6370400372e5a937a5b85ff5d8cb39c
phattran <phatth1203@gmail.com>
2020-07-23 08:54:28 +0700

    wtf

--- utils.py
+++ utils.py
@@ -111,7 +111,7 @@ def pickle_load(path):
         print("Loading data from {}".format(path))
         with open(path, "rb") as f:
             data = pickle.load(f)
-             print('load data successfully'.format(path))
+            print('load data successfully'.format(path))
             return data
     except Exception as e:
         print(str(e))
