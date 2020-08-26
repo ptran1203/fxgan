547bcdc91bc2b0209e287ca0ac0e53a4f9b0aaad
phattran <phatth1203@gmail.com>
2020-08-26 14:41:13 +0700

    notle

--- data_augmentation/model.py
+++ data_augmentation/model.py
@@ -499,7 +499,7 @@ def run(mode, test_data ,experiments = 1, frozen_block=[],
                     outputs = train_model.get_layer('side_out').get_output_at(-1),
                     name="center_loss"
                 )
-                scatter_plot(x_train, y_train, embbeding_model, 'train', 'tsne')
+                scatter_plot(x_train, y_train, embbeding_model, 'train', 'tsne', legend=False)
 
 
 
